b547ab5068e3ba6fe4b560a1ac5503e8f7182c38
ald-ac <muriasaldair@hotmail.com>
2021-07-06 15:08:21 -0500

    Verificando respuestas asi como contando aciertos para mostrarle al usuario puntaje y que respuestas contesto mal

--- js/app.js
+++ js/app.js
@@ -40,82 +40,28 @@ function comprobarRespuestas(e) {
         if(pregunta.value === '') {
             alert('¡Contesta todas las preguntas!');
             return true;
+        } else {
+            comprobarRespuestas();
         }
     });
+}
+
+function comprobarRespuestas() {
+    let aciertos = 0;
+    for (let i = 0; i < respuestas.length; i++) {
+        if(respuestas[i] === preguntasHTML[i].value.toUpperCase().trim()) {
+            preguntasHTML[i].style.backgroundColor = 'green';
+            preguntasHTML[i].r1.style.color = '#ffffff';
+            aciertos++; 
+        } else {
+            preguntasHTML[i].style.backgroundColor = 'red';
+            preguntasHTML[i].style.color = '#ffffff';
+        }
+    }
+    alert('¡Has acertado ' + aciertos + ' de 7!');
 }
 
 function reiniciarJuego() {
     location.reload();
     formulario.reset();
 }
-
-// function recibirRespuestas(e) {
-//     e.preventDefault();
-//     if(r1.value != '' && r2.value != '' && r3.value != '' && r4.value != '' && r5.value != '' && r6.value != '' && r7.value != '') {
-//         comprobarRespuestas();
-//     } else {
-//         alert('¡Contesta todas las peliculas!');
-//     }
-// }
-
-// function comprobarRespuestas() {
-//     let aciertos=0;
-//     if(r1.value.toUpperCase().trim() === 'HARRY POTTER') {
-//         r1.style.backgroundColor = 'green';
-//         r1.style.color = '#ffffff';
-//         aciertos++;
-//     } else {
-//         r1.style.backgroundColor = 'red';
-//         r1.style.color = '#ffffff';
-//     }
-//     if(r2.value.toUpperCase().trim() === 'KUNG FU PANDA') {
-//         r2.style.backgroundColor = 'green';
-//         r2.style.color = '#ffffff';
-//         aciertos++;
-//     } else {
-//         r2.style.backgroundColor = 'red';
-//         r2.style.color = '#ffffff';
-//     }
-//     if(r3.value.toUpperCase().trim() === 'EL PLANETA DE LOS SIMIOS') {
-//         r3.style.backgroundColor = 'green';
-//         r3.style.color = '#ffffff';
-//         aciertos++;
-//     } else {
-//         r3.style.backgroundColor = 'red';
-//         r3.style.color = '#ffffff';
-//     }
-//     if(r4.value.toUpperCase().trim() === 'POLLITOS EN FUGA') {
-//         r4.style.backgroundColor = 'green';
-//         r4.style.color = '#ffffff';
-//         aciertos++;
-//     } else {
-//         r4.style.backgroundColor = 'red';
-//         r4.style.color = '#ffffff';
-//     }
-//     if(r5.value.toUpperCase().trim() === 'CENICIENTA') {
-//         r5.style.backgroundColor = 'green';
-//         r5.style.color = '#ffffff';
-//         aciertos++;
-//     } else {
-//         r5.style.backgroundColor = 'red';
-//         r5.style.color = '#ffffff';
-//     }
-//     if(r6.value.toUpperCase().trim() === 'EL SEÑOR DE LOS ANILLOS') {
-//         r6.style.backgroundColor = 'green';
-//         r6.style.color = '#ffffff';
-//         aciertos++;
-//     } else {
-//         r6.style.backgroundColor = 'red';
-//         r6.style.color = '#ffffff';
-//     }
-//     if(r7.value.toUpperCase().trim() === 'TED') {
-//         r7.style.backgroundColor = 'green';
-//         r7.style.color = '#ffffff';
-//         aciertos++;
-//     } else {
-//         r7.style.backgroundColor = 'red';
-//         r7.style.color = '#ffffff';
-//     }
-//     alert('¡Has acertado ' + aciertos + ' de 7!');
-// }
-
