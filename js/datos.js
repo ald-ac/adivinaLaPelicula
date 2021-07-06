36ede285b6aaf07adc15a9ba8f9915be59c5b494
ald-ac <muriasaldair@hotmail.com>
2021-07-06 14:17:55 -0500

    Generando preguntas en el DOM a partir de arreglos

---
+++ js/datos.js
@@ -0,0 +1,20 @@
+//emojis y nombre de una pelicula respectivamente
+let emojis = [
+    '&#128102;&#127997;&#128083;&#9889;',
+    '&#128074;&#128165;&#128060;',
+    '&#127758;&#128053;&#128018;&#128053;',
+    '&#128019;&#128036;&#127939;&#128168;',
+    '&#128120;&#127997;&#9201;&#128096;&#127875;',
+    '&#128104;&#127995;&#128141;&#128141;&#128141;',
+    '&#128059;&#128684;&#127866;&#127866;'
+]
+
+let respuestas = [
+    'HARRY POTTER',
+    'KUNG FU PANDA',
+    'EL PLANETA DE LOS SIMIOS',
+    'POLLITOS EN FUGA',
+    'CENICIENTA',
+    'EL SEÑOR DE LOS ANILLOS',
+    'TED',
+]
